063a23e68722d02ae5fafdb916c481bd1296e110
DimasVeliz <trevor90094@yandex.com>
2023-06-15 17:36:30 +0200

    base project uploaded

---
+++ src/main/java/com/boosting/code/authMService/AuthMServiceApplication.java
@@ -0,0 +1,13 @@
+package com.boosting.code.authMService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthMServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthMServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boosting/code/authMService/AuthMServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boosting.code.authMService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthMServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
